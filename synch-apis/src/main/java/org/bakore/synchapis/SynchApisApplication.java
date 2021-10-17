f3cf34b52a87be78a87cd88289a0e581aa433f61
dhananjaybakore <dhananjaybakore@gmail.com>
2021-10-16 23:19:57 -0400

    Created Synch API Module.

---
+++ synch-apis/src/main/java/org/bakore/synchapis/SynchApisApplication.java
@@ -0,0 +1,13 @@
+package org.bakore.synchapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynchApisApplication.class, args);
+    }
+
+}
---
+++ synch-apis/src/test/java/org/bakore/synchapis/SynchApisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bakore.synchapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
